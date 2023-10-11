6561b3fd27bd97b28e1ecd08d14ebc38e18dae90
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2023-10-11 16:00:57 -0700

    complete responsivity

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
 // Nav bar actions
 const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
+
+const brownName = document.querySelector("#brown");
+const brownDisc = document.querySelector("#brownDisc");
+const straightDisc = document.querySelector("#straight");
+const dhaliDisc = document.querySelector("#dhali");
+
+const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+document.addEventListener("DOMContentLoaded", function() {
+  if(viewportWidth < 450){
+    brownName.style.fontSize = "25px";
+
+    brownDisc.style.fontSize = "16px";
+    straightDisc.style.fontSize = "16px";
+    dhaliDisc.style.fontSize = "16px";
+  }
+});
+
+
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
 
